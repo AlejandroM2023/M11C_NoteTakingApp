fc67e6842b450274e4a9067deb59fc2a1c790867
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-01 10:21:52 -0500

    port change for render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-const PORT = 3001;
+const PORT =  process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -10,6 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.listen(PORT);
+app.listen(PORT, () => console.log('live'));
 
 
