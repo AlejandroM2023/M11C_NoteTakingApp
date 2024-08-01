c14d5f096ff48a1f8561258bc6285c54343ec5b0
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-31 22:30:43 -0500

    api routing and id creationg added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
-const path = require('path');
+const fs = require('fs');
+const notes = require('../../db/db.json');
+const idGen = require('uniqid');
+
+
+
+router.get('/notes', (req,res)=> {
+    res.send(notes);
+    console.log(notes);
+});
+
+router.post('/notes', (req,res)=> {
+    req.body.id = idGen();
+    notes.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => err?console.log(err):console.log('Note saved'));
+    res.status(201).json(req.body);
+});
 
 
 module.exports = router;
